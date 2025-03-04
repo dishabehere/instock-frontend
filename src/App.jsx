3c9932fe21756f333774cf4e17d39b55c97d89af
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-04 15:14:36 -0500

    react router set up and basic routes done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        
+        <Route path="/warehouses" element={<WarehouseList />} />
+        <Route path="/warehouses/:warehouseId" element={<WarehouseDetails />} />
+        <Route path="/warehouses/:warehouseId/inventories" element={<WarehouseInventories />} />
+        <Route path="/inventories/:inventoryId" element={<InventoryDetails />} />
       </Routes>
     </Router>
   );
