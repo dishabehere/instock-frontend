acb4ab04cbce8b98f47302b0dfe25dc942062ce2
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-05 17:31:18 -0500

    Add new font files, change font file

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,12 +1,34 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import search from "../../assets/icons/search-24px.svg";
-import "./WarehouseList.scss"
+import searchIcon from "../../assets/icons/search-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+import "./WarehouseList.scss";
 
+function WarehouseList() {
+    const [warehouses, setWarehouses] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
-function WarehouseList(){
-return (
-    <>
+    useEffect(() => {
+        const fetchWarehouses = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/warehouses`);
+                console.log(response.data);
+                setWarehouses(response.data);
+            } catch (error) {
+                console.error("Error fetching warehouses:", error);
+            }
+        };
+
+        fetchWarehouses();
+    }, []);
+
+    // // Filter warehouses based on search query
+    // const filteredWarehouses = warehouses.filter((warehouse) =>
+    //     warehouse.name.toLowerCase().includes(searchQuery.toLowerCase())
+    // );
+
+    return (
         <section className="warehouse-list">
             <div className="warehouse-list__header">
                 <h1 className="warehouse-list__heading">Warehouses</h1>
@@ -15,22 +37,42 @@ return (
                         className="warehouse-list__search-text"
                         type="text"
                         placeholder="Search..."
-                        // value={query}
-                        // onChange={(e) => setQuery(e.target.value)}
-                    />        
-                    <img className="warehouse-list-search" src={search} alt="search logo" />
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <img className="warehouse-list__search-icon" src={searchIcon} alt="Search" />
                 </div>
-                <button className="warehouse-list__button">
-                    + Add New Warehouse
-                </button>
+                <button className="warehouse-list__button">+ Add New Warehouse</button>
             </div>
 
-            <div>
-                
+            <div className="warehouse-list__container">
+                {warehouses.map((warehouse) => (
+                    <div key={warehouse.id} className="warehouse-list__item">
+                        <div className="warehouse-list__details">
+                            <div className="warehouse-list__column">
+                                <h4 className="warehouse-list__label">WAREHOUSE</h4>
+                                <p className="warehouse-list__name">{warehouse.warehouse_name} &gt;</p>
+                                <h4 className="warehouse-list__label">ADDRESS</h4>
+                                <p className="warehouse-list__address">{warehouse.address}</p>
+                            </div>
+
+                            <div className="warehouse-list__column">
+                                <h4 className="warehouse-list__label">CONTACT NAME</h4>
+                                <p className="warehouse-list__contact">{warehouse.contactName}</p>
+                                <h4 className="warehouse-list__label">CONTACT INFORMATION</h4>
+                                <p className="warehouse-list__info">{warehouse.contactInfo}</p>
+                            </div>
+                        </div>
+
+                        <div className="warehouse-list__actions">
+                            <img className="warehouse-list__icon" src={deleteIcon} alt="Delete" />
+                            <img className="warehouse-list__icon" src={editIcon} alt="Edit" />
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
-    </>
-);
+    );
 }
 
 export default WarehouseList;
