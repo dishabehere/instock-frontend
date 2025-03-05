a6472edab21190cf6f5e0a0c53144a0c4bbbda92
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-05 12:24:23 -0500

    uncommented routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* <Route path="/warehouses/:warehouseId" element={<WarehouseDetailsPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/warehouses/:warehouseId" element={<WarehouseDetailsPage />} />
         <Route path="/warehouses/add" element={<WarehouseFormPage />} />
         <Route path="/warehouses/edit" element={<WarehouseFormPage />} />
         <Route path="/inventories" element={<InventoriesListPage />} />
         <Route path="/inventories/:inventoryId" element={<InventoryDetailsPage />} />
         <Route path="/inventories/add" element={<InventoriesFormPage />} />
-        <Route path="/inventories/edit" element={<InventoriesFormPage />} /> */}
+        <Route path="/inventories/edit" element={<InventoriesFormPage />} />
       </Routes>
       <Footer/>
     </Router>
