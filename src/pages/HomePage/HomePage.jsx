7772a5a549d820f034c6f913d7e18a3bc5d9d93e
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-04 23:08:07 -0500

    Create the WarehouseList component files and basic structure for Home Page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import "./HomePage.scss";
+
+
+return (
+    <>
+        <Header />
+
+        <WarehouseList />
+
+        {/* <Footer /> */}
+    </>
+)
