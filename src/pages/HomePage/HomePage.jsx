f411d246dba445b3dd644bb5211f0e7bfc09d5a8
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-05 23:18:35 -0500

    Add shadow to the warehouseList, change margin-top for footer and remove header and footer from the home page.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,10 @@
-// import Header from "../../components/Header/Header";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import "./HomePage.scss";
 
 function HomePage() {
 return (
     <>
-        {/* <Header /> */}
-
         <WarehouseList />
-
-        {/* <Footer /> */}
     </>
 );
 }
