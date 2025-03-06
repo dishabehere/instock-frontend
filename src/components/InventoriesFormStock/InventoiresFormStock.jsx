d40c995f3ac579a81187b762b40ab69024f2410f
candiceojleung <candice.leung@live.ca>
2025-03-06 14:09:38 -0500

    fix bug and change filie from js to jsx

---
+++ src/components/InventoriesFormStock/InventoiresFormStock.jsx
@@ -0,0 +1,69 @@
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+function InventoriesFormStock() {
+  const location = useLocation();
+  const isEditPage = location.pathname.includes("/edit");
+  const [stockStatus, setStockStatus] = useState("instock");
+
+  const handleStatusChange = (event) => {
+    setStockStatus(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="stock">
+        <h2 className="stock__availability">Item Availability</h2>
+        <p className="stock__label">Status</p>
+        <label className="stock__selector">
+          <input
+            type="radio"
+            name="stock_status"
+            value="instock"
+            className="stock__instock"
+            checked={stockStatus === "instock"}
+            onChange={handleStatusChange}
+          />
+          In stock
+        </label>
+        <label className="stock__selector">
+          <input
+            type="radio"
+            name="stock_status"
+            value="outofstock"
+            className="stock__outofstock"
+            checked={stockStatus === "outofstock"}
+            onChange={handleStatusChange}
+          />
+          Out of Stock
+        </label>
+      </div>
+      {stockStatus === "instock" && (
+        <>
+          <p className="stock__quantity">Quantity</p>
+          <label className="stock__field">
+            <input type="text" name="quantity" className="stock__number" />
+          </label>
+        </>
+      )}
+
+      <p className="stock__label">Warehouse</p>
+      <label className="stock__field">
+        <select
+          name="warehouse-location"
+          className="stock__locations"
+        ></select>
+      </label>
+      <div>Cancel</div>
+      <div className="stock__submit">
+        {isEditPage ? (
+          <div className="stock__save">Save</div>
+        ) : (
+          <div className="stock__add">+ Add Item</div>
+        )}
+      </div>
+    </form>
+  );
+}
+
+export default InventoriesFormStock;
