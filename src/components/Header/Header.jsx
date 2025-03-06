8e171efc7b53d6a89fc3e3bd7a2df1d8980c538b
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-06 09:57:33 -0500

    added separate active classes for warehouse and inventory

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
   return (
-    <header className='header__container'>
+    <header className="header__container">
       <img
         className="header__logo"
         src="../src/assets/logo/InStock-Logo.svg"
         alt="header logo of two arrows in opposite directions"
-        ></img>
-      <nav className='nav-bar__container'>
-        <NavLink className={({ isActive }) => 
-            isActive ? 'nav-bar__link nav-bar__link--active' : 'nav-bar__link'
-          } to="/">
-          <h2 className='nav-bar__title'>Warehouse</h2>
+      ></img>
+      <nav className="nav-bar__container">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "nav-bar__link nav-bar__link--active nav-bar__link--warehouse-active"
+              : "nav-bar__link"
+          }
+          to="/"
+        >
+          <h2 className="nav-bar__title">Warehouse</h2>
         </NavLink>
-        <NavLink to="/inventories" className={({ isActive }) => 
-            isActive ? 'nav-bar__link nav-bar__link--active' : 'nav-bar__link'
-          } >
-          <h2 className='nav-bar__title'>Inventory</h2>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "nav-bar__link nav-bar__link--active nav-bar__link--inventory-active"
+              : "nav-bar__link"
+          }
+          to="/inventories"
+        >
+          <h2 className="nav-bar__title">Inventory</h2>
         </NavLink>
       </nav>
-
     </header>
   );
 }
