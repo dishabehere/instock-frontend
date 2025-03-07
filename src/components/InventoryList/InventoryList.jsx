bf8dce63a590913a2436a44b77887ea63097dee6
candiceojleung <candice.leung@live.ca>
2025-03-07 04:45:25 -0500

    build out row content

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -46,5 +46,83 @@ function InventoryList() {
           <button className="inventory-list__button">+ Add New Item</button>
         </div>
       </div>
+      
+      <div className="inventory-list__index">
+        <div className="inventory-list__title">
+          <h4 className="inventory-list__title-text">Inventory Item</h4>
+          <img className="inventory-list__sort" src={sort} alt="sort" />
+        </div>
+        <div className="inventory-list__title">
+          <h4 className="inventory-list__title-text">Category</h4>
+          <img className="inventory-list__sort" src={sort} alt="sort" />
+        </div>
+        <div className="inventory-list__title">
+          <h4 className="inventory-list__title-text">Status</h4>
+          <img className="inventory-list__sort" src={sort} alt="sort" />
+        </div>
+        <div className="inventory-list__title">
+          <h4 className="inventory-list__title-text">Qty</h4>
+          <img className="inventory-list__sort" src={sort} alt="sort" />
+        </div>
+        <div className="inventory-list__title">
+          <h4 className="inventory-list__title-text">Warehouse</h4>
+          <img className="inventory-list__sort" src={sort} alt="sort" />
+        </div>
+        <h4 className="inventory-list__title-text">Actions</h4>
+      </div>
+      <div className="inventory-list__container">
+        {inventories.map((inventory) => (
+          <div key={inventory.id} className="inventory-list__item">
+            <div className="inventory-list__details">
+              <div className="inventory-list__content">
+                <div className="inventory-list__info">
+                  <h4 className="inventory-list__label">Inventory Item</h4>
+                  <div className="inventory-list__name">
+                    <p className="inventory-list__text inventory-list__text--name">
+                      <Link to={`/api/inventories/${inventory.id}`}>
+                        {inventory.item_name}{" "}
+                      </Link>
+                    </p>
+                    <img
+                      className="inventory-list__chevron"
+                      src={chevron}
+                      alt="Chevron Icon"
+                    />
+                  </div>
+                  </div>
+                <div className="inventory-list__info">
+                  <h4 className="inventory-list__label">Category</h4>
+                  <p className="inventory-list__text inventory-list__text--category">
+                    {inventory.category}
+                  </p>
+                </div>
+              </div>
+
+              <div className="inventory-list__content inventory-list__content--status">
+                <div className="inventory-list__info">
+                  <h4 className="inventory-list__label">Status</h4>
+                  <p
+                    className={`inventory-list__text--status ${
+                      inventory.status === "In Stock"
+                        ? "in-stock"
+                        : "out-of-stock"
+                    }`}
+                  >
+                    {inventory.status}
+                  </p>
+                </div>
+                <div className="inventory-list__info">
+                  <h4 className="inventory-list__label">QTY</h4>
+                  <p className="inventory-list__text">{inventory.quantity}</p>
+                </div>
+                <div className="inventory-list__info">
+                  <h4 className="inventory-list__label">Warehouse</h4>
+                  <p className="inventory-list__text">
+                    {inventory.warehouse_name}
+                  </p>
+                </div>
+              </div>
+            </div>
+
       </section>
   )}
