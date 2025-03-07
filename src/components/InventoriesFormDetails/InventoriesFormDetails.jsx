536b70c500c6a66817f8394f1734b86efcc8bd86
candiceojleung <candice.leung@live.ca>
2025-03-06 19:44:35 -0500

    create category dropdown and set up form field population when on edit page

--- src/components/InventoriesFormDetails/InventoriesFormDetails.jsx
+++ src/components/InventoriesFormDetails/InventoriesFormDetails.jsx
@@ -1,25 +1,48 @@
 import { useState, useEffect } from "react";
-import { fetchInventoryItem } from "../../utils/inventoryUtils";
+import axios from "axios";
 
-function InventoriesFormDetails({ id }) {
+function InventoriesFormDetails({ id }) {  
   const [inventory, setInventory] = useState({
     item_name: "",
     description: "",
     category: "",
   });
+  const [categories, setCategories] = useState([]);
+
+  const handleCategoryChange = (e) => {
+    setInventory(prev => ({ ...prev, category: e.target.value }));
+  };
 
   useEffect(() => {
-    fetchInventoryData();
-  }, [id]);
+    fetchCategories();
+    if (id) fetchInventoryItem(id);  
+  }, [id]);  
 
-  async function fetchInventoryData() {
+  async function fetchCategories() {
     try {
-      const data = await fetchInventoryItem(id);
-      setInventory(data);
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/inventories`
+      );
+      const extractedCategories = data.map((inventory) => inventory.category);
+      const uniqueCategories = [...new Set(extractedCategories)];
+
+      setCategories(uniqueCategories);
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching categories:", error);
     }
   }
+
+  async function fetchInventoryItem(id) {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/inventories/${id}`
+      );
+      setInventory(data);  
+    } catch (error) {
+      console.error("Error fetching inventory item:", error);
+    }
+  }
+
   return (
     <form>
       <div className="details">
@@ -28,29 +51,31 @@ function InventoriesFormDetails({ id }) {
         <label className="details__fields">
           <input
             type="text"
-            name="name"
+            name="item_name"
             className="details__name"
             value={inventory.item_name}
+            readOnly
           />
         </label>
         <p className="details__label">Description</p>
         <label className="details__fields">
           <textarea
-            type="text"
             name="description"
             value={inventory.description}
             className="details__description"
+            readOnly
           />
         </label>
         <p className="details__label">Category</p>
         <div>
           <label className="details__dropdown">
-            <select
-              name="category"
+            <select 
+              name="category" 
               className="details__category"
               value={inventory.category}
+              onChange={handleCategoryChange}
             >
-              <option value="">{inventory.category}</option>
+              <option value="">Select a category</option>
               {categories.map((category, index) => (
                 <option key={index} value={category}>
                   {category}
--- src/pages/InventoriesFormPage/InventoriesFormPage.jsx
+++ src/pages/InventoriesFormPage/InventoriesFormPage.jsx
@@ -1,12 +1,14 @@
 import InventoriesFormDetails from "../../components/InventoriesFormDetails/InventoriesFormDetails";
 import InventoriesFormStock from "../../components/InventoriesFormStock/InventoriesFormStock";
 import InventoriesFormHeader from "../../components/InventoriesFormHeader/InventoriesFormHeader";
+import { useParams } from "react-router-dom";
 
 export default function InventoriesFormPage() {
+  const { id } = useParams();
   return (
     <>
       <InventoriesFormHeader />
-      <InventoriesFormDetails />
+      <InventoriesFormDetails id={id} />
       <InventoriesFormStock />
     </>
   );
