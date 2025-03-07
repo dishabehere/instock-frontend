5e5d5a742e580263e3399d122528bffddc73e460
candiceojleung <candice.leung@live.ca>
2025-03-07 02:29:10 -0500

    add border to bottom of header so that tablet and desktop styling makes sense

--- src/components/InventoriesFormHeader/InventoriesFormHeader.jsx
+++ src/components/InventoriesFormHeader/InventoriesFormHeader.jsx
@@ -6,7 +6,7 @@ function InventoriesFormHeader() {
   const isEditPage = location.pathname.includes('/edit');
 
   return (
-    <div className="form__header">
+    <section className="form__header">
       <Link to="/inventories">
         <img
           className="form__img"
@@ -17,7 +17,7 @@ function InventoriesFormHeader() {
       <h2 className={`form__title ${isEditPage ? 'form__title--edit' : 'form__title--add'}`}>
         {isEditPage ? 'Edit Inventory Item' : 'Add New Inventory Item'}
       </h2>
-    </div>
+    </section>
   );
 }
 
