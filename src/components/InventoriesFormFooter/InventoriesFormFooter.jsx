a5e0e6ca3b41c1e05ec24f8748d1355a7dac5b66
candiceojleung <candice.leung@live.ca>
2025-03-07 01:49:57 -0500

    add footer component to account for styling in tablet and desktop

---
+++ src/components/InventoriesFormFooter/InventoriesFormFooter.jsx
@@ -0,0 +1,23 @@
+import { useLocation } from "react-router-dom";
+import "./InventoriesFormFooter.scss"
+
+function InventoriesFormFooter() {
+  const location = useLocation();
+  const isEditPage = location.pathname.includes("/edit");
+  return (
+    <>
+      <div className="buttons">
+        <button className="buttons__cancel">Cancel</button>
+        <div className="buttons__submit">
+          {isEditPage ? (
+            <button className="buttons__save">Save</button>
+          ) : (
+            <button className="buttons__add">+ Add Item</button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default InventoriesFormFooter;
