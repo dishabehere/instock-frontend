8f55dec3296427670b4378cfd14472e010fe5ceb
candiceojleung <candice.leung@live.ca>
2025-03-06 22:21:51 -0500

    style overall form page

--- src/pages/InventoriesFormPage/InventoriesFormPage.jsx
+++ src/pages/InventoriesFormPage/InventoriesFormPage.jsx
@@ -2,14 +2,15 @@ import InventoriesFormDetails from "../../components/InventoriesFormDetails/Inve
 import InventoriesFormStock from "../../components/InventoriesFormStock/InventoriesFormStock";
 import InventoriesFormHeader from "../../components/InventoriesFormHeader/InventoriesFormHeader";
 import { useParams } from "react-router-dom";
+import "./InventoriesFormPage.scss"
 
 export default function InventoriesFormPage() {
   const { id } = useParams();
   return (
-    <>
+    <section className="inventoryForm">
       <InventoriesFormHeader />
       <InventoriesFormDetails id={id} />
       <InventoriesFormStock id={id}/>
-    </>
+      </section>
   );
 }
