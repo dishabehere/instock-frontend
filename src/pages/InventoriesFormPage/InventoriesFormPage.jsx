90c5071d4195e568082e11e13ac0d547dc0be20a
candiceojleung <candice.leung@live.ca>
2025-03-07 02:30:20 -0500

    add form footer, and change styling to account for tablet mode

--- src/pages/InventoriesFormPage/InventoriesFormPage.jsx
+++ src/pages/InventoriesFormPage/InventoriesFormPage.jsx
@@ -1,6 +1,7 @@
 import InventoriesFormDetails from "../../components/InventoriesFormDetails/InventoriesFormDetails";
 import InventoriesFormStock from "../../components/InventoriesFormStock/InventoriesFormStock";
 import InventoriesFormHeader from "../../components/InventoriesFormHeader/InventoriesFormHeader";
+import InventoriesFormFooter from "../../components/InventoriesFormFooter/InventoriesFormFooter";
 import { useParams } from "react-router-dom";
 import "./InventoriesFormPage.scss"
 
@@ -9,8 +10,11 @@ export default function InventoriesFormPage() {
   return (
     <section className="inventoryForm">
       <InventoriesFormHeader />
+      <div className= "inventoryForm__main">
       <InventoriesFormDetails id={id} />
       <InventoriesFormStock id={id}/>
+      </div>
+      <InventoriesFormFooter />
       </section>
   );
 }
