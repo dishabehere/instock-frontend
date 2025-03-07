17bda482fc50ac63ffa895cec8b3443ec22d23b2
candiceojleung <candice.leung@live.ca>
2025-03-07 18:17:57 -0500

    add in conditional classes for form validation

--- src/components/InventoriesFormDetails/InventoriesFormDetails.jsx
+++ src/components/InventoriesFormDetails/InventoriesFormDetails.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./InventoriesFormDetails.scss"
+import "./InventoriesFormDetails.scss";
 
-function InventoriesFormDetails({ id }) {  
+function InventoriesFormDetails({ id }) {
   const [inventory, setInventory] = useState({
     item_name: "",
     description: "",
@@ -10,14 +10,22 @@ function InventoriesFormDetails({ id }) {
   });
   const [categories, setCategories] = useState([]);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInventory((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   const handleCategoryChange = (e) => {
-    setInventory(prev => ({ ...prev, category: e.target.value }));
+    setInventory((prev) => ({ ...prev, category: e.target.value }));
   };
 
   useEffect(() => {
     fetchCategories();
-    if (id) fetchInventoryItem(id);  
-  }, [id]);  
+    if (id) fetchInventoryItem(id);
+  }, [id]);
 
   async function fetchCategories() {
     try {
@@ -26,7 +34,6 @@ function InventoriesFormDetails({ id }) {
       );
       const extractedCategories = data.map((inventory) => inventory.category);
       const uniqueCategories = [...new Set(extractedCategories)];
-
       setCategories(uniqueCategories);
     } catch (error) {
       console.error("Error fetching categories:", error);
@@ -38,14 +45,14 @@ function InventoriesFormDetails({ id }) {
       const { data } = await axios.get(
         `${import.meta.env.VITE_BASE_URL}/api/inventories/${id}`
       );
-      setInventory(data);  
+      setInventory(data);
     } catch (error) {
       console.error("Error fetching inventory item:", error);
     }
   }
 
   return (
-    <form className="details">
+    <form className="details" onSubmit={handleSubmit}>
       <div className="details__section">
         <h3 className="details__header">Item Details</h3>
         <h4 className="details__label">Item Name</h4>
@@ -53,9 +60,13 @@ function InventoriesFormDetails({ id }) {
           <input
             type="text"
             name="item_name"
-            className="details__name"
+            className={`details__name ${
+              submit && inventory.item_name.trim() === ""
+                ? "details__name--invalid"
+                : ""
+            }`}
             value={inventory.item_name}
-            readOnly
+            onChange={handleInputChange}
           />
         </label>
         <h4 className="details__label">Description</h4>
@@ -63,22 +74,36 @@ function InventoriesFormDetails({ id }) {
           <textarea
             name="description"
             value={inventory.description}
-            className="details__description"
-            readOnly
+            className={`details__description ${
+              submit && inventory.description.trim() === ""
+                ? "details__description--invalid"
+                : ""
+            }`}
+            onChange={handleInputChange}
           />
         </label>
         <h4 className="details__label">Category</h4>
         <div>
           <label className="details__dropdown">
-            <select 
-              name="category" 
-              className="details__category"
+            <select
+              name="category"
+              className={`details__category ${
+                submit &&
+                (inventory.category === "" ||
+                  inventory.category === "Please Select")
+                  ? "details__category--invalid"
+                  : ""
+              }`}
               value={inventory.category}
               onChange={handleCategoryChange}
             >
               <option value="">Please Select</option>
               {categories.map((category, index) => (
-                <option className="details__options" key={index} value={category}>
+                <option
+                  className="details__options"
+                  key={index}
+                  value={category}
+                >
                   {category}
                 </option>
               ))}
