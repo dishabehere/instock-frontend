2fc71c78d7fae3b97a518f3134cd601fed76038a
candiceojleung <candice.leung@live.ca>
2025-03-08 02:10:54 -0500

    add in utils file, and fix bug where in stock and out of stuck couldn't be toggled

--- src/components/InventoriesFormStock/InventoriesFormStock.jsx
+++ src/components/InventoriesFormStock/InventoriesFormStock.jsx
@@ -1,32 +1,46 @@
 import "./InventoriesFormStock.scss";
 import InventoriesFormError from "../InventoriesFormError/InventoriesFormError";
-import {useState, useEffect} from "react";
-import {useLocation} from "react-router-dom";
-import axios from "axios";
+import { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { getInventoryItem, getAllInventories } from "../../utils/apiUtils";
 
 function InventoriesFormStock({ formData, handleInputChange, errors }) {
-
-  const [warehouse, setWarehouse] = useState([]);
+  const [warehouseName, setWarehouseName] = useState([]);
   const location = useLocation();
   const isEditPage = location.pathname.includes("/edit");
-
+  const { id } = useParams();
 
   useEffect(() => {
-    fetchWarehouses();
+    fetchWarehouseName();
   }, []);
 
-  async function fetchWarehouses() {
+  useEffect(() => {
+    if (isEditPage && id) {
+      fetchInventoryItem(id);
+    }
+  }, [isEditPage, id]);
+
+  async function fetchWarehouseName() {
     try {
-      const { data } = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/api/inventories`
+      const data = await getAllInventories();
+      const extractedWarehouseName = data.map(
+        (inventory) => inventory.warehouse_name
       );
-      const extractedWarehouse = data.map((inventory) => inventory.warehouse_name);
-      const uniqueWarehouse = [...new Set(extractedWarehouse)];
-      setWarehouse(uniqueWarehouse);
+      const uniqueWarehouseName = [...new Set(extractedWarehouseName)];
+      setWarehouseName(uniqueWarehouseName);
     } catch (error) {
-      console.error("Error fetching Warehouse:", error);
+      console.error("Error fetching Warehouse Name", error);
     }
   }
+
+  const fetchInventoryItem = async (id) => {
+    try {
+      const data = await getInventoryItem(id);
+      setFormData(data);
+    } catch (error) {
+      console.error("Error fetching inventory item:", error);
+    }
+  };
 
   return (
     <div className="stock">
@@ -35,7 +49,11 @@ function InventoriesFormStock({ formData, handleInputChange, errors }) {
           <h2 className="stock__availability">Item Availability</h2>
           <h4 className="stock__label">Status</h4>
           <div className="stock__statuses">
-            <label className={`stock__selector ${formData.status === "instock" ? "stock__selector--selected" : ""}`}>
+            <label
+              className={`stock__selector ${
+                formData.status === "instock" ? "stock__selector--selected" : ""
+              }`}
+            >
               <input
                 type="radio"
                 name="status"
@@ -46,7 +64,13 @@ function InventoriesFormStock({ formData, handleInputChange, errors }) {
               />
               In stock
             </label>
-            <label className={`stock__selector ${formData.status === "outofstock" ? "stock__selector--selected" : ""}`}>
+            <label
+              className={`stock__selector ${
+                formData.status === "outofstock"
+                  ? "stock__selector--selected"
+                  : ""
+              }`}
+            >
               <input
                 type="radio"
                 name="status"
@@ -67,10 +91,13 @@ function InventoriesFormStock({ formData, handleInputChange, errors }) {
               <input
                 type="number"
                 name="quantity"
-                className={`stock__number ${errors.quantity ? "stock__number--invalid" : ""}`}
+                className={`stock__number ${
+                  errors.quantity ? "stock__number--invalid" : ""
+                }`}
                 value={formData.quantity}
                 onChange={handleInputChange}
                 min="1"
+                placeholder={!isEditPage ? "Enter quantity" : ""}
               />
               {errors.quantity && <InventoriesFormError />}
             </label>
@@ -81,11 +108,20 @@ function InventoriesFormStock({ formData, handleInputChange, errors }) {
         <label className="stock__field">
           <select
             name="warehouse_name"
-            className={`stock__locations ${errors.warehouse_name ? "stock__locations--invalid" : ""}`}
+            className={`stock__locations ${
+              errors.warehouse_name ? "stock__locations--invalid" : ""
+            }`}
             value={formData.warehouse_name}
             onChange={handleInputChange}
           >
-            <option className="stock__select" value="">Please select</option>
+            <option className="stock__select" value="">
+              Please select
+            </option>
+            {warehouseName.map((warehouse, index) => (
+              <option key={index} value={warehouse}>
+                {warehouse}
+              </option>
+            ))}
           </select>
           {errors.warehouse_name && <InventoriesFormError />}
         </label>
