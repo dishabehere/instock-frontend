290830d91967f1c3b614111989bf7632280b7de2
candiceojleung <candice.leung@live.ca>
2025-03-08 10:15:09 -0500

    remove console logs

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -1,13 +1,12 @@
 import "./InventoryItemDetails.scss";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function InventoryItemDetails() {
   const { id } = useParams();
   const [item, setItem] = useState("null");
-  
 
   useEffect(() => {
     const fetchItem = async () => {
@@ -15,6 +14,7 @@ function InventoryItemDetails() {
         const response = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/api/inventories/${id}`
         );
+
         setItem(response.data);
       } catch (error) {
         console.error("Error fetching inventory items:", error);
@@ -34,33 +34,46 @@ function InventoryItemDetails() {
               src="../src/assets/icons/arrow_back-24px.svg"
               alt="arrow-icon"
             ></img>
-            <h3 className="item__title">{item.item_name}</h3>
+            <p className="item__header">{item.item_name}</p>
           </div>
         </Link>
         <div className="item__icon">
-          <Link to="/inventories"><img
-            className="item__edit"
-            src="../src/assets/icons/edit-white-24px.svg"
-            alt="edit icon"
-          />
-           </Link>
+          <Link to="/inventories/${id}/edit">
+            <img
+              className="item__edit"
+              src="../src/assets/icons/edit-white-24px.svg"
+              alt="edit icon"
+            />
+          </Link>
         </div>
       </div>
       <div className="item__details">
         <div className="item__descriptors">
-          <h3 className="item__description">Item Description</h3>
+          <h4 className="item__title">Item title:</h4>
           <p className="item__text">{item.description}</p>
-          <h3 className="item__category">Category</h3>
+          <h4 className="item__title">Category:</h4>
           <p className="item__text">{item.category}</p>
         </div>
         <div className="item__quantities">
-          <div className="item__amoount">
-            <h3 className="item__status">Status</h3>
-            <p className="item__stock">{item.stock}</p>
-            <h3 className="item__quantity">Quantity</h3>
-            <p className="item__text">{item.quantity}</p>
+          <div className="item__amount">
+            <div className="item__textwrap">
+              <h4 className="item__title">Status:</h4>
+              <p
+                className={`item__text ${
+                  item.status === "In Stock"
+                    ? "item__text--instock"
+                    : "item__text--outofstock"
+                }`}
+              >
+                {item.status}
+              </p>
+            </div>
+            <div className="item__textwrap">
+              <h4 className="item__title">Quantity:</h4>
+              <p className="item__text">{item.quantity}</p>
+            </div>
           </div>
-          <h3 className="item__warehouse">Warehouse</h3>
+          <h4 className="item__title">Warehouse:</h4>
           <p className="item__text">{item.warehouse_name}</p>
         </div>
       </div>
