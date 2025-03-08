a7ac0ba22322a6ba9ca30c9cd87ca118396ad59d
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-07 21:59:11 -0500

    added onclick for warehouse list page

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -6,6 +6,7 @@ import editIcon from "../../assets/icons/edit-24px.svg";
 import chevron from "../../assets/icons/chevron_right-24px.svg";
 import sort from "../../assets/icons/sort-24px.svg";
 import "./WarehouseList.scss";
+import { Link } from "react-router-dom"; 
 
 function WarehouseList() {
     const [warehouses, setWarehouses] = useState([]);
@@ -76,7 +77,8 @@ function WarehouseList() {
                                 <div className="warehouse-list__info"> 
                                     <h4 className="warehouse-list__label">Warehouse</h4>
                                     <div className="warehouse-list__name">
-                                        <p className="warehouse-list__text warehouse-list__text--name">{warehouse.warehouse_name}</p>   
+                                        <Link to={`/warehouses/${warehouse.id}`} className="warehouse-list__text warehouse-list__text--name">
+                                            <p>{warehouse.warehouse_name}</p></Link>
                                         <img className="warehouse-list__chevron" src={chevron} alt="Chevron Icon" />
                                     </div>
                                 </div>
