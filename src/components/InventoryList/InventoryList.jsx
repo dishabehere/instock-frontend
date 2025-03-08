98b60c986aa12ec5e9a34e27ec437e01a0d4fd4c
candiceojleung <candice.leung@live.ca>
2025-03-08 02:09:00 -0500

    add utils function to retrieve inventory list page

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -7,6 +7,7 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import chevron from "../../assets/icons/chevron_right-24px.svg";
 import sort from "../../assets/icons/sort-24px.svg";
+import { getAllInventories } from "../../utils/apiUtils";
 
 function InventoryList() {
   const [inventories, setInventories] = useState([]);
@@ -14,10 +15,8 @@ function InventoryList() {
   useEffect(() => {
     const fetchInventories = async () => {
       try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_BASE_URL}/api/inventories`
-        );
-        setInventories(response.data);
+        const data = await getAllInventories(); 
+        setInventories(data); 
       } catch (error) {
         console.error("Error fetching inventories:", error);
       }
