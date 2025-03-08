7441058fbd62c83ac71fee012ddbd639c1dbeba6
candiceojleung <candice.leung@live.ca>
2025-03-08 02:36:11 -0500

    change path so that there aren't any duplication

--- src/components/InventoriesFormStock/InventoriesFormStock.jsx
+++ src/components/InventoriesFormStock/InventoriesFormStock.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { getInventoryItem, getAllInventories } from "../../utils/apiUtils";
 
-function InventoriesFormStock({ formData, handleInputChange, errors }) {
+function InventoriesFormStock({ formData, handleInputChange,  setFormData, errors }) {
   const [warehouseName, setWarehouseName] = useState([]);
   const location = useLocation();
   const isEditPage = location.pathname.includes("/edit");
